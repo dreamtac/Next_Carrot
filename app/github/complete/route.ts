d3504df7244109094d7e24eaed832ee509404eb7
Dongwha <dreamtac@gmail.com>
2024-10-09 21:41:40 +0900

    feat: github/complete/route.ts
    액세스 토큰 받아오기 완료

---
+++ app/github/complete/route.ts
@@ -0,0 +1,30 @@
+import { notFound } from 'next/navigation';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest) {
+    const code = request.nextUrl.searchParams.get('code');
+    if (!code) {
+        return notFound();
+    }
+    const accessTokenParams = new URLSearchParams({
+        client_id: process.env.GITHUB_CLIENT_ID!,
+        client_secret: process.env.GITHUB_CLIENT_SECRET!,
+        code: code,
+    }).toString();
+    const accessTokenURL = `https://github.com/login/oauth/access_token?${accessTokenParams}`;
+
+    const accessTokenResponse = await (
+        await fetch(accessTokenURL, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+            },
+        })
+    ).json();
+
+    if ('error' in accessTokenResponse) {
+        return new NextResponse(null, { status: 400 });
+    }
+
+    return NextResponse.json({ accessTokenResponse });
+}
