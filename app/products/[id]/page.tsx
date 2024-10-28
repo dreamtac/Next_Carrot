74a8c4712f83d25b0b83347995735250d97f5682
Dongwha <dreamtac@gmail.com>
2024-10-28 19:43:29 +0900

    feat: product 유효성 검사, product db 연결

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,8 +1,47 @@
-async function getProduct() {
-    await new Promise(resolve => setTimeout(resolve, 60000));
+import db from '@/lib/db';
+import { getSession } from '@/lib/session';
+import { notFound } from 'next/navigation';
+
+async function getIsOwner(userId: number) {
+    // 현재 로그인된 유저가 업로드한 제품의 소유자인지 체크하는 함수
+    const session = await getSession();
+    if (session.id === userId) {
+        console.log('오너임');
+        return true;
+    }
+    console.log('오너 아님');
+    return false;
 }
 
-export default async function ProductDetail({ params: { id } }: { params: { id: string } }) {
-    const product = await getProduct();
+async function getProduct(id: number) {
+    const product = await db.product.findUnique({
+        where: {
+            id: id,
+        },
+        include: {
+            // 관계를 가지고 있는 테이블(user)에서 username과 avatar를 가져옴.
+            user: {
+                select: {
+                    username: true,
+                    avatar: true,
+                },
+            },
+        },
+    });
+    console.log(product);
+    return product;
+}
+
+export default async function ProductDetail({ params }: { params: { id: string } }) {
+    const id = Number(params.id); //id는 숫자
+    if (isNaN(id)) {
+        //id에 숫자가 아닌 문자가 들어오면 NaN
+        return notFound();
+    }
+    const product = await getProduct(id); //db에서 가져온 product
+    if (!product) {
+        return notFound();
+    }
+    const isOwner = await getIsOwner(product.userId); // product의 userId로 해당 제품의 오너인지 체크
     return <span>Product Detail! id:{id}</span>;
 }
