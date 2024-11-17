5f321a19d84d98d3361000f2bacc62fca1dcc52a
Dongwha <dreamtac@gmail.com>
2024-11-17 19:57:04 +0900

    feat: modal (intercept, parallel)

--- app/@parallel/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Parallel() {
-    return <h1>병렬라우팅</h1>;
-}
--- app/layout.tsx
+++ app/layout.tsx
@@ -12,20 +12,10 @@ export const metadata: Metadata = {
     description: 'Sell and buy all the things',
 };
 
-export default function RootLayout({
-    children,
-    parallel, // parallel을 명시적으로 가져옵니다.
-}: {
-    children: React.ReactNode;
-    parallel: React.ReactNode; // parallel의 타입을 ReactNode로 지정.
-}) {
-    console.log('Parallel slot:', parallel);
+export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en" className="bg-neutral-900">
-            <body className={`${inter.className} text-white max-w-screen-sm mx-auto`}>
-                {parallel}
-                {children}
-            </body>
+            <body className={`${inter.className} text-white max-w-screen-sm mx-auto`}>{children}</body>
         </html>
     );
 }
